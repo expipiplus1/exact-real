da04858d2a27ef433284bfb388ac9c4aae86a114
Joe Hermaszewski <git@monoid.al>
2015-11-17 17:18:50 +0000

    More accurate test description

--- test/RealFrac.hs
+++ test/RealFrac.hs
@@ -9,18 +9,20 @@ import Test.QuickCheck.Checkers (EqProp, (=-=))
 import Test.Tasty (testGroup, TestTree)
 import Test.Tasty.QuickCheck (testProperty, Arbitrary)
 
+-- TODO: Test the other functions
 realFrac :: forall a. (Arbitrary a, EqProp a, Show a, RealFrac a) =>
             a -> TestTree
 realFrac x = testGroup "Test RealFrac instance" ts
   where ts = [ properFractionLaws "properFraction laws" x ]
 
+-- | This tests a slightly different law for n having the same sign as x
 properFractionLaws :: forall a. (Arbitrary a, EqProp a, Show a, RealFrac a) =>
                       String -> a -> TestTree
 properFractionLaws s _ = testGroup s ts
   where ts = [ testProperty "x = n + f"
                             (\x -> let (n, f) = properFraction (x :: a)
                                    in x =-= fromInteger n + f)
-             , testProperty "n has same sign"
+             , testProperty "n has same sign or is zero"
                             (\x -> let (n, _) = properFraction (x :: a)
                                    in n == 0 || sign x == sign (n::Int))
              , testProperty "abs f < 1"

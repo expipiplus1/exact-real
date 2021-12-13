8031e14d26d1c36350d2bd55ba624a6fe41d5cf4
Ellie Hermaszewska <git@monoid.al>
2021-12-13 15:43:56 +0800

    pin nixpkgs

--- default.nix
+++ default.nix
@@ -1,4 +1,8 @@
-{ nixpkgsSrc ? <nixpkgs>, pkgs ? import nixpkgsSrc { }, compiler ? null }:
+{ nixpkgsSrc ? builtins.fetchTarball {
+  url =
+    "https://github.com/NixOS/nixpkgs/archive/e675946ecde5606c505540de2024e2732bae4185.tar.gz"; # nixos-unstable
+  sha256 = "1xnqhz0wxkgkwpwkal93k5rj72j39pvck542i9jyxh9bm25rc4j5";
+}, pkgs ? import nixpkgsSrc { }, compiler ? null }:
 
 let
   haskellPackages = if compiler == null then
